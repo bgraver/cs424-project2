8e818b91b37c0d43eff3d8045f3b19a0814e13ac
brandon <brandon@DESKTOP-909GNUO.localdomain>
2020-02-27 16:59:04 -0600

    changing title of app

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Making sure the branch exists"),
+    titlePanel("Project 2"),
 # 
 #     # Sidebar with a slider input for number of bins 
 #     sidebarLayout(
